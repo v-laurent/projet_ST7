be35bfcdde6c8a4e562d3bd7a1daadc3f3da19ec
Laurent Valentin <valentin.laurent@student-cs.fr>
2021-03-03 09:58:20 +0100

    adding reading_data unit

--- projet_ST7/phase_1/main.py
+++ projet_ST7/phase_1/main.py
@@ -1,37 +1,5 @@
-from classes import TEmployee
-from classes import TTask
+from classes import *
 from utils import *
-import csv
-from os.path import dirname, join
-import pandas as pd
+from reading_data import *
 
-#reading data
-current_dir = dirname(__file__)
-file_path = current_dir + "/InstancesV1/InstanceBordeauxV1.xlsx"
-xls = pd.ExcelFile(file_path)
-
-#employees
-employees_sheet = pd.read_excel(xls, 'Employees')
-skillToRank = {skill : i for i,skill in enumerate( set(employees_sheet["Skill"]) )}
-skillToRank['other'] = len(skillToRank)
-
-employees = []
-for index, row in employees_sheet.iterrows():
-    workingStartTime = dateToMinute( row["WorkingStartTime"] )
-    workingEndTime = dateToMinute( row["WorkingEndTime"] )
-    skill = skillToRank[ row["Skill"] ]
-    employees.append( TEmployee(row["EmployeeName"], row["Latitude"], row["Longitude"], skill, row["Level"], workingStartTime, workingEndTime) )
-
-#tasks
-task_sheet = pd.read_excel(xls, 'Tasks')
-tasks = []
-for index, row in task_sheet.iterrows():
-    openingTime = dateToMinute( row["OpeningTime"] )
-    closingTime = dateToMinute( row["ClosingTime"] )
-    if row["Skill"] in skillToRank.keys(): 
-        skill = skillToRank[ row["Skill"] ] 
-    else: skill = skillToRank["other"]
-    tasks.append( TTask(row["TaskId"], row["Latitude"], row["Longitude"], row["TaskDuration"], skill, row["Level"], openingTime, closingTime) )
-
-
-
+employees, tasks = readingData("Bordeaux")
---
+++ projet_ST7/phase_1/reading_data.py
@@ -0,0 +1,36 @@
+from os.path import dirname
+import pandas as pd
+from utils import *
+from classes import *
+
+
+def readingData(country):
+
+    current_dir = dirname(__file__)
+    file_path = current_dir + "/InstancesV1/Instance" + country + "V1.xlsx"
+    xls = pd.ExcelFile(file_path)
+
+    #employees
+    employees_sheet = pd.read_excel(xls, 'Employees')
+    skillToRank = {skill : i for i,skill in enumerate( set(employees_sheet["Skill"]) )}
+    skillToRank['other'] = len(skillToRank)
+
+    employees = []
+    for index, row in employees_sheet.iterrows():
+        workingStartTime = dateToMinute( row["WorkingStartTime"] )
+        workingEndTime = dateToMinute( row["WorkingEndTime"] )
+        skill = skillToRank[ row["Skill"] ]
+        employees.append( TEmployee(row["EmployeeName"], row["Latitude"], row["Longitude"], skill, row["Level"], workingStartTime, workingEndTime) )
+
+    #tasks
+    task_sheet = pd.read_excel(xls, 'Tasks')
+    tasks = []
+    for index, row in task_sheet.iterrows():
+        openingTime = dateToMinute( row["OpeningTime"] )
+        closingTime = dateToMinute( row["ClosingTime"] )
+        if row["Skill"] in skillToRank.keys(): 
+            skill = skillToRank[ row["Skill"] ] 
+        else: skill = skillToRank["other"]
+        tasks.append( TTask(row["TaskId"], row["Latitude"], row["Longitude"], row["TaskDuration"], skill, row["Level"], openingTime, closingTime) )
+
+    return employees, tasks
