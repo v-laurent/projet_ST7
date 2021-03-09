ecfa55043b76cc9c963cfda2fbdc1e976cd2edeb
Laurent Valentin <valentin.laurent@student-cs.fr>
2021-03-09 15:29:28 +0100

    version fonctionnelle et propre

--- projet_ST7/phase_1/main.py
+++ projet_ST7/phase_1/main.py
@@ -4,136 +4,60 @@
 from classes import *
 from utils import *
 from reading_data import *
+from model import *
+
 # Modules de base
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-# Module relatif à Gurobi
+
 from gurobipy import *
 
 ##***************************** Reading Data 
 
 employees, tasks = readingData("Bordeaux")
-number_of_employees = len(employees)
-number_of_tasks = len(tasks)
-
+number_of_employees,  number_of_tasks = len(employees), len(tasks)
+depot = TTask(0,employees[0].Latitude, employees[0].Longitude, 0, None, 0,480,1440)
+employees = [None] + employees
+tasks = [depot] + tasks
+    
 ##***************************** Model 
 
-m=Model('PL_phase_1')
+DELTA, T = best_solution(employees,tasks)
 
-#Definition des variables de decision, en majuscules
-DELTA={(i,j,k): m.addVar(vtype=GRB.BINARY,name=f'DELTA_{i}_{j}_{k}') 
-    for i in range(0,number_of_tasks+1) 
-    for j in range(0,number_of_tasks+1)
-    for k in range(1,number_of_employees+1)}
+##****************************   plot 
 
-T={(i,k): m.addVar(vtype=GRB.INTEGER,lb=480, ub=1440, name=f'T_{i}_{k}') 
-    for i in range(1,number_of_tasks+1) 
-    for k in range(1,number_of_employees+1)}
+latitudes=[[] for employee in range(number_of_employees+1)]
+longitudes=[[] for employee in range(number_of_employees+1)]
+task_numbers=[[] for employee in range(number_of_employees+1)]
 
-#Definition des variables utiles pour la modelisation du probleme
-d={(i,j,k): distance(tasks[i-1],tasks[j-1]) for i in range(1,len(tasks)+1) for j in range(1,len(tasks)+1) for k in range(1,len(employees)+1)}
+for k in range(1,number_of_employees+1):
+    T_f = []
+    for i in range(number_of_tasks+1):
+        T_f.append(T[(k,i)])
+    T_indices = np.argsort(T_f)
 
-for k in range(1,len(employees)+1):
-    for j in range(1,len(tasks)+1):
-        d[0,j,k]=distance(employees[k-1],tasks[j-1])
-        d[j,0,k]=distance(employees[k-1],tasks[j-1])
-    d[0,0,k]=0
+    for i in T_indices:
+        for j in T_indices:
+            if DELTA[(i,j,k)]==1:
+                if i!=0 and j!=0:
+                    print("distance{}-{}".format(i,j),int(distance(tasks[i],tasks[j])))
+                    latitudes[k].append(tasks[i].Latitude)
+                    longitudes[k].append(tasks[i].Longitude)
+                    task_numbers[k].append(i)
+                elif i==0 and j!=0:
+                    print("distance{}-{}".format(i,j),int(distance(employees[k],tasks[j])),"distance au depot")
+                    latitudes[k].append(employees[k].Latitude)
+                    longitudes[k].append(employees[k].Longitude)
+                    task_numbers[k].append(i)
+                elif j==0 and i!=0:
+                    print("distance{}-{}".format(i,j),int(distance(tasks[i],employees[k])),"distance au depot")
+                    latitudes[k].append(tasks[i].Latitude)
+                    longitudes[k].append(tasks[i].Longitude)
+                    task_numbers[k].append(i)
+    latitudes[k].append(employees[k].Latitude)
+    longitudes[k].append(employees[k].Longitude)
+    task_numbers[k].append(0)
 
-
-t={i: tasks[i-1].TaskDuration for i in range(1,len(tasks)+1)}
-
-s_employees={i: employees[i-1].WorkingStartTime for i in range(1,len(employees)+1)}
-
-e_employees={i: employees[i-1].WorkingEndTime for i in range(1,len(employees)+1)}
-
-s_tasks={i: tasks[i-1].OpeningTime for i in range(1,len(tasks)+1)}
-
-e_tasks={i: tasks[i-1].ClosingTime for i in range(1,len(tasks)+1)}
-
-level_employee={i: employees[i-1].Level for i in range(1,len(employees)+1)}
-
-level_task={i: tasks[i-1].Level for i in range(1,len(tasks)+1)}
-
-# Definition des contraintes 
-Realisation_constr=dict()
-
-for task1 in range(1,number_of_tasks+1):
-    Realisation_constr[task1]=m.addConstr(quicksum(DELTA[(task1,task2,employee)] 
-                                        for task2 in range(0,len(tasks)+1) 
-                                        for employee in range(1,len(employees)+1))==1, 
-                                        name='DELTA_{}'.format(task1))
-                      
-
-Unique_task_constr=dict()
-M=1440
-for k in range(1,len(employees)+1):
-    for j in range(1,len(tasks)+1):
-        for i in range(1,len(tasks)+1):
-            print("trajet{}{}".format(i-1,j-1),trajet(tasks[i-1],tasks[j-1]))
-            Unique_task_constr[(i,j,k)]=m.addConstr(T[(j,k)]>=T[(i,k)]+t[i]+trajet(tasks[i-1],tasks[j-1])-(1-DELTA[(i,j,k)])*M,name=f'Unique_{i}_{j}_{k}')
-
-Available_task_constr_sup=dict()
-Available_task_constr_inf=dict()
-for i in range(1,len(tasks)+1):
-    for k in range(1,len(employees)+1):
-        Available_task_constr_sup[(i,k)]=m.addConstr(T[(i,k)]<=e_tasks[i]-t[i])
-        Available_task_constr_inf[(i,k)]=m.addConstr(T[(i,k)]>=s_tasks[i])
-
-Available_employee_constr_sup=dict()
-Available_employee_constr_inf=dict()
-for i in range(1,len(tasks)+1):
-    for k in range(1,len(employees)+1):
-        Available_employee_constr_sup[(i,k)]=m.addConstr(T[(i,k)]<=e_employees[k]-t[i]-trajet(employees[k-1],tasks[i-1]))
-        Available_employee_constr_inf[(i,k)]=m.addConstr(T[(i,k)]>=s_employees[k])
-
-Level_constr=dict()
-for j in range(1,len(tasks)+1):
-    Level_constr[j]=m.addConstr(quicksum(DELTA[(i,j,k)]*(level_employee[k]-level_task[j])
-                                for i in range(1,len(tasks)+1)
-                                for k in range(1,len(employees)+1))>=0)
-
-Start_employee=dict()
-for k in range(1,len(employees)+1):
-    Start_employee[k]=m.addConstr(quicksum(DELTA[(0,j,k)]
-                                    for j in range(1,len(tasks)+1))==1)
-                                    
-End_employee=dict()
-for k in range(1,len(employees)+1):
-    End_employee[k]=m.addConstr(    quicksum(DELTA[(i,0,k)] 
-                                    for i in range(1,len(tasks)+1))==1)
-
-Flow_constr=dict()
-for k in range(1,len(employees)+1):
-    for t in range(0,len(tasks)+1):
-        Flow_constr[(k,t)]=m.addConstr(quicksum(DELTA[(i,t,k)]
-                                        for i in range(0,len(tasks)+1))==
-                                        quicksum(DELTA[(t,j,k)]
-                                        for j in range(0,len(tasks)+1)))
-
-m.setObjective(quicksum(DELTA[(i,j,k)]*d[(i,j,k)] for i in range(0, len(tasks)+1)
-                                                for j in range(0,len(tasks)+1)
-                                                for k in range(1,len(employees)+1)),GRB.MINIMIZE)
-
-# -- Mise à jour du modèle  --
-m.update()
-m.optimize()
-latitudes=[[] for employee in range(len(employees))]
-longitudes=[[] for employee in range(len(employees))]
-task_numbers=[[] for employee in range(len(employees))]
-for k in range(1,len(employees)+1):
-    latitudes[k-1].append(employees[k-1].Latitude)
-    longitudes[k-1].append(employees[k-1].Longitude)
-    task_numbers[k-1].append(0)
-    for i in range(0,len(tasks)+1):
-        for j in range(0,len(tasks)+1):
-            if DELTA[(i,j,k)].x==1:
-                if i!=0:
-                    print("distance{}{}".format(i,j),distance(tasks[i-1],tasks[j-1]))
-                    latitudes[k-1].append(tasks[i-1].Latitude)
-                    longitudes[k-1].append(tasks[i-1].Longitude)
-                    task_numbers[k-1].append(i)
-    latitudes[k-1].append(employees[k-1].Latitude)
-    longitudes[k-1].append(employees[k-1].Longitude)
-    task_numbers[k-1].append(0)
-draw(latitudes,longitudes,task_numbers,'bordeauxtest')
+draw(latitudes,longitudes,task_numbers,'bordeauxtest',DELTA)
+fichier_texte(DELTA,T,employees,number_of_tasks,'bordeaux')
--- projet_ST7/phase_1/utils.py
+++ projet_ST7/phase_1/utils.py
@@ -23,31 +23,31 @@ colors=['black','red','green','blue','yellow','cyan','orange','slategray','lemon
 color_code=dict()
 color_code={i:color for (i,color) in enumerate(colors)}
 
-def draw(latitude_list_list,longitude_list_list,task_numbers,name):
-    mean_latitude=sum(latitude_list_list[0])/len(latitude_list_list[0])
-    mean_longitude=sum(longitude_list_list[0])/len(longitude_list_list[0])
-    gmap1 = gmplot.GoogleMapPlotter(mean_latitude, mean_longitude,  13 )
-    for employee in range(len(latitude_list_list)):
-        gmap1.plot(latitude_list_list[employee],longitude_list_list[employee], color_code[employee],edge_width=2.5)
-        for point in range(len(latitude_list_list[employee])):
+def draw(latitude_list_list,longitude_list_list,task_numbers,name,DELTA):
+    mean_latitude=sum(latitude_list_list[1])/len(latitude_list_list[1])
+    mean_longitude=sum(longitude_list_list[1])/len(longitude_list_list[1])
+    gmap1 = gmplot.GoogleMapPlotter(mean_latitude, mean_longitude,  10)
+    for employee in range(1,len(latitude_list_list)):
+        gmap1.plot(latitude_list_list[employee],longitude_list_list[employee], color_code[employee-1],edge_width=2.5)
+        for point in range(0,len(latitude_list_list[employee])):
             gmap1.marker(latitude_list_list[employee][point],longitude_list_list[employee][point],title="{}".format(task_numbers[employee][point]))
-    gmap1.draw( "{}".format(name) )
+    gmap1.draw( "{}".format(name))
 
 def trajet(depart,arrivee):
     return (3.6/(50*60))*distance(depart,arrivee)
 
-def fichier_texte(DELTA,T,employees,number_of_tasks):
-    texte=open('bordeaux.txt','w')
+def fichier_texte(DELTA,T,employees,number_of_tasks,titre):
+    texte=open(f'{titre}.txt','w')
     #rajouter le cas ou la tache n est pas realisee
     resultats=[['taskId','performed','employeeName','startTime']]
 
     number_of_employees=len(employees)
-    for i in range(0,number_of_tasks+1): 
-        for j in range(0,number_of_tasks+1):
-            for k in range(1,number_of_employees+1):
-                if DELTA[(i,j,k)].x==1:
+    for i in range(0,number_of_tasks): 
+        for j in range(0,number_of_tasks):
+            for k in range(1,number_of_employees):
+                if DELTA[(i,j,k)]==1:
                     if i!= 0:
-                        resultats.append([f'T{i}',1,employees[k-1].EmployeeName,T[(i,k)].x])
+                        resultats.append([f'T{i}',1,employees[k].EmployeeName,T[(k,i)]])
     for line in (resultats):
         texte.write("{};{};{};{};\n".format(line[0],line[1],line[2],line[3]))
     print(resultats)
