ee9ebb1fbf6933af52cdf30c0a9a521c50eb3dc0
Andriamasy Yandi <yandi.andriamasy@student-cs.fr>
2021-03-17 16:38:52 +0100

    deuxieme_modif_main

--- projet_ST7/phase_2/main.py
+++ projet_ST7/phase_2/main.py
@@ -18,20 +18,41 @@ from gurobipy import *
 country = "Bordeaux"
 employees, tasks = readingData(country)
 number_of_employees,  number_of_tasks = len(employees), len(tasks)
-depots = [TTask(0,employees[k].Latitude, employees[k].Longitude, 0, 0,"",480,1440,[],0,k)
+depots = [TTask(0,employees[k].Latitude, employees[k].Longitude,0,"",0,480,1440,[],0,k)
         for k in range(1,number_of_employees+1)]
-def sous_taches(Tasks):
-    NewTasks=[]
-    
+def sous_taches(tasks):
+    new_tasks=[]
+    for i in range(1,number_of_tasks+1):
+        if len(tasks[i].Unavailabilities)==0:
+            new_tasks.append(tasks[i])
+        else:
+            start=tasks[i].OpeningTime
+            end=tasks[i].Unavailabilies[0].startTime
+            new_tasks.append(TTask(tasks[i].TaskId,tasks[i].Latitude,tasks[i].Longitude,
+                            tasks[i].TaskDuration,tasks[i].Skill, tasks[i].Level,
+                            start, end, [],len(tasks[i].Unavailabilities)))
+            for sous_tache in range(len(tasks[i].Unavailabilities)):
+                start=tasks[i].Unavailabilities[sous_tache].End
+                if sous_tache == len(tasks[i].Unavailabilities)-1:
+                    end=tasks[i].ClosingTime
+                else:
+                    end=tasks[i].Unavailabilies[sous_tache+1].Start
+                new_tasks.append(TTask(tasks[i].TaskId,tasks[i].Latitude,tasks[i].Longitude,
+                            tasks[i].TaskDuration,tasks[i].Skill, tasks[i].Level,
+                            start, end, [],len(tasks[i].Unavailabilities)))
+            
+
 employees_unavailability=[]
 for k in range(1,number_of_employees+1):
     if len(employees[k].Unavalaibilities)!=0: #if the employee has unavailabilities
         employees_unavailability.append(TTask(-1,employees[k].Unavalaibilities[0].Latitude, employees[k].Unavailabilies[0].Longitude, 
                         employees[k].Unavalaibilities[0].ClosingTime-employees[k].Unavalaibilities[0].ClosingTime,
-                        0,"",employees[k].Unavalaibilities[0].OpeningTime, employees[k].Unavalaibilities[0].ClosingTime,
-                        [],k))
+                        "",0,employees[k].Unavalaibilities[0].OpeningTime, employees[k].Unavalaibilities[0].ClosingTime,
+                        [],0,k))
+    else:
+        employees_unavailability.append(None)
 
-new_tasks = [None]+ [depots] + tasks
+new_tasks = [None]+ [depots] + employees_unavailability+ sous_taches(tasks)
 
 ##***************************** Model 
 
