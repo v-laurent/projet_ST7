6a18c96b70e9932049edfd1d241a01e0c846cb1d
Anouar OUSSALAH <anouaroussalah@Anouars-MacBook-Pro.local>
2021-03-17 14:55:08 +0100

    modif reading-data.py

--- projet_ST7/phase_2/reading_data.py
+++ projet_ST7/phase_2/reading_data.py
@@ -7,8 +7,17 @@ from classes import *
 def readingData(country):
 
     current_dir = dirname(__file__)
-    file_path = current_dir + "/InstancesV1/Instance" + country + "V1.xlsx"
+    file_path = current_dir + "/InstancesV2/Instance" + country + "V2.xlsx"
     xls = pd.ExcelFile(file_path)
+
+    #employees_unavailabilities
+    employees_unavailabilities_sheet = pd.read_excel(xls, 'Employees Unavailibilities')
+    employees_unavailabilities = []
+    
+    for index, row in employees_unavailabilities_sheet.iterrows():
+        Start = dateToMinute( row["Start"] )
+        End = dateToMinute( row["End"] )
+        employees_unavailabilities.append("ca vient" )
 
     #employees
     employees_sheet = pd.read_excel(xls, 'Employees')
@@ -17,12 +26,19 @@ def readingData(country):
     employees = []
     
     for index, row in employees_sheet.iterrows():
-        print(index,row)
         workingStartTime = dateToMinute( row["WorkingStartTime"] )
         workingEndTime = dateToMinute( row["WorkingEndTime"] )
         skill = skillToRank[ row["Skill"] ]
         employees.append( TEmployee(row["EmployeeName"], row["Latitude"], row["Longitude"], skill, row["Level"], workingStartTime, workingEndTime) )
 
+      #tasks_unavailabilities
+    task_unavailabilities_sheet = pd.read_excel(xls, 'Tasks Unavailabilities')
+    tasks_unavailabilities = []
+    for index, row in task_sheet_unavailabilities.iterrows():
+        Start = dateToMinute( row["Start"] )
+        End = dateToMinute( row["End"] )
+        tasks_unavailabilities.append( "idem" )
+        
     #tasks
     task_sheet = pd.read_excel(xls, 'Tasks')
     tasks = []
@@ -34,4 +50,6 @@ def readingData(country):
         else: skill = skillToRank["other"]
         tasks.append( TTask(row["TaskId"], row["Latitude"], row["Longitude"], row["TaskDuration"], skill, row["Level"], openingTime, closingTime) )
 
+    
+    
     return employees, tasks
