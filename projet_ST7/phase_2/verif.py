66d4e976dd50f34078ea5aaa26186dd2a015d009
Alexis RICHARD <alexis.richard@student-cs.fr>
2021-03-23 23:37:17 +0100

    Gantt - indisponibilités des employés

--- projet_ST7/phase_2/verif.py
+++ projet_ST7/phase_2/verif.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 #################################################
 country = 'Bordeaux'
 dossier_txt = 'fichiers_txt_phase2'
-version = 'V1ByV1'
+version = 'V2ByV2'
 #################################################
 # précision choisie pour les warning (en minutes)
 epsilon = 1
@@ -175,6 +175,7 @@ def gantt_diagram(country):
     task_times=[[] for _ in range(len(employees))]
     travel_times=[[] for _ in range(len(employees))]
     lunch_times=[[] for _ in range(len(employees))]
+    unavailabilities_times=[[] for _ in range(len(employees))]
 
     decision = pd.read_csv(f"{dossier_txt}/Solution{country}{version}.txt", sep=';', nrows=number_of_tasks)
     decision = decision[['taskId','performed','employeeName','startTime']]
@@ -216,11 +217,17 @@ def gantt_diagram(country):
         lunch_time = lunch[lunch['employeeName']==employee_name].loc[:,'lunchBreakStartTime'].values[0]
         lunch_times[k].append((lunch_time, 60))
 
+        # indisponibilités
+        for unavailability in employees[k].Unavailabilities:
+            unavailabilities_times[k].append((unavailability.Start, unavailability.End-unavailability.Start))
+
+
     colors=['pink','orange','blue']
     for i in range(len(employees)):
         gnt.broken_barh(task_times[i],(10*(i+1)-1,2), facecolors=('tab:{}'.format(colors[i])))
         gnt.broken_barh(travel_times[i],(10*(i+1)-2,0.5),facecolors=('tab:red'))
         gnt.broken_barh(lunch_times[i],(10*(i+1)-3,0.5),facecolors=('tab:green'))
+        gnt.broken_barh(unavailabilities_times[i],(10*(i+1)-3,0.5),facecolors=('tab:blue'))
     
     plt.vlines([12*60, 14*60], 0, 10*(number_of_employees+1), 'red', 'dashed', alpha=0.5)
     plt.savefig('gantt_phase2/{}.png'.format(country))
