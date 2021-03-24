4d3fcb99f69980dd2dc0319a0f826fab94c0b5ce
Anouar OUSSALAH <anouaroussalah@Anouars-MacBook-Pro.local>
2021-03-24 18:34:18 +0100

    comments in english ;)

--- projet_ST7/phase_2/verif.py
+++ projet_ST7/phase_2/verif.py
@@ -10,12 +10,12 @@ country = 'Poland'
 dossier_txt = 'fichiers_txt_phase2'
 version = 'V2ByV2'
 #################################################
-# précision choisie pour les warning (en minutes)
+# chosen accuracy for the warning (in minutes)
 epsilon = 1
 
 
 def verification(country, epsilon=0.05):
-    employees,tasks = readingData(country)          # le dépôt n'est pas dans les tâches
+    employees,tasks = readingData(country)          # the depot is not in tasks
     number_of_employees = len(employees)-1
     number_of_tasks = len(tasks)-1
     
@@ -25,10 +25,10 @@ def verification(country, epsilon=0.05):
     lunch = pd.read_csv(f"{dossier_txt}/Solution{country}{version}.txt", sep=';', skiprows=number_of_tasks+1 ,nrows=number_of_employees)
     lunch = lunch[['employeeName','lunchBreakStartTime']]
 
-    # Pourcentage de tâches réalisées
+    # Pourcentage of accomplished tasks
     print("Pourcentage de tâches réalisées : {} %".format(round((decision['performed'].sum())/number_of_tasks*100,1)))
 
-    # Contraintes
+    # Constraints
     for i in range(1, number_of_tasks+1):
         task_id = tasks[i].TaskId
         T_i_startTime = decision[decision['taskId']==task_id].loc[:,'startTime'].values
@@ -43,9 +43,9 @@ def verification(country, epsilon=0.05):
         
         k=1
         while employees[k].EmployeeName != employee_name:
-            k+=1                                                # indice de l'employé effectuant la tâche i
+            k+=1                                                # index of the employee accomplishing the task
 
-        # 1- disponibilité des tâches            
+        # 1- tasks availabilities            
         if T_i_startTime < tasks[i].OpeningTime:
             print(f"ERROR 1.1 : tâche {task_id} réalisée par {employee_name} avant l'ouverture de la tâche")
         if T_i_startTime > tasks[i].ClosingTime:
@@ -56,7 +56,7 @@ def verification(country, epsilon=0.05):
                 if T_i_startTime + tasks[i].TaskDuration > unavailability.Start and T_i_startTime < unavailability.End:
                     print(f"ERROR 1.4 : tâche {task_id} réalisée par {employee_name} pendant une période d'indisponibilité de la tâche")
 
-        # 2- disponibilité des employés
+        # 2- employees availabilities
         if T_i_startTime < employees[k].WorkingStartTime:
             print(f"ERROR 2.1 : {employee_name} réalise la tache {task_id} avant le début de son service")
         if T_i_startTime > employees[k].WorkingEndTime:
@@ -67,7 +67,7 @@ def verification(country, epsilon=0.05):
             if T_i_startTime + tasks[i].TaskDuration > unavailability.Start and T_i_startTime < unavailability.End:
                 print(f"ERROR 2.4 : {employee_name} réalise la tache {task_id} pendant sa période d'indisponibilité")
 
-        # 3- compétences
+        # 3- skills
         if employees[k].Skill != tasks[i].Skill:
             print(f"ERROR 3.1 : compétence {employees[k].Skill} manquante")
         else :
@@ -75,25 +75,25 @@ def verification(country, epsilon=0.05):
                 print(f"ERROR 3.2 : compétence n°{tasks[k].Skill} de niveau {tasks[i].Level} requis par {employee_name} (niveau {employees[k].Level} appris)")
 
 
-    # Vérification du trajet des employés
+    # Verify the employees travels
     for k in range(1,number_of_employees+1):
         employee_name = employees[k].EmployeeName
         tasks_id = decision[decision['employeeName']==employee_name].loc[:,'taskId'].values
         T_i_startTime = decision[decision['employeeName']==employee_name].loc[:,'startTime'].values
 
-        # vérification que l'employé travaille
+        # verify that employees work
         if len(T_i_startTime) == 0:
             print(f"{employee_name} n'effectue aucune tâche")
             continue
         
-        sorted_indices = np.argsort(T_i_startTime)        # indices uniquement des tâches effectuées par le technicien k
-        sorted_indices_bis = []                             # utilisables sur tasks (ensemble de toutes les tâches)
+        sorted_indices = np.argsort(T_i_startTime)         # indexs of tasks accomplished only by employee k
+        sorted_indices_bis = []                             # set of all tasks
         for i in sorted_indices:
             for j in range(1,number_of_tasks+1):
                 if tasks[j].TaskId == tasks_id[i]:
                     sorted_indices_bis.append(j)
 
-        # 4- pause midi
+        # 4- Lunch break time
         lunch_time = lunch[lunch['employeeName']==employee_name].loc[:,'lunchBreakStartTime'].values
         if len(lunch_time) == 0:
             print(f"ERROR 4.1 : {employee_name} ne fait pas de pause repas") 
@@ -102,7 +102,7 @@ def verification(country, epsilon=0.05):
         elif lunch_time > 14*60:
             print(f"ERROR 4.3 : {employee_name} fait sa pause repas trop tard, à {lunch_time[0]}")
     
-        # 5- trajet dépôt / première tâche
+        # 5- depot/work travel
         i0 = sorted_indices[0]
         i0_bis = sorted_indices_bis[0]
         eps = T_i_startTime[i0] - (employees[k].WorkingStartTime + trajet(employees[k], tasks[i0_bis]))
@@ -112,10 +112,10 @@ def verification(country, epsilon=0.05):
             if eps > epsilon:
                 print(f"warning 5.2 : {employee_name} part de son dépôt {round(eps,2)}min trop tard")
         
-        # 6- temporalité des tâches
+        # 6- time coherence of tasks
         for i in range(1,len(sorted_indices)):
-            i1 = sorted_indices[i-1]                # tâche de départ
-            i2 = sorted_indices[i]                  # tâche d'arrivée
+            i1 = sorted_indices[i-1]                # starting task
+            i2 = sorted_indices[i]                  # arrival task
             i1_bis = sorted_indices_bis[i-1]
             i2_bis = sorted_indices_bis[i]
             eps = T_i_startTime[i2] - (T_i_startTime[i1] + tasks[i1_bis].TaskDuration + trajet(tasks[i1_bis],tasks[i2_bis]))
@@ -125,7 +125,7 @@ def verification(country, epsilon=0.05):
                 if eps > epsilon:
                     print(f"warning 6.2 : {employee_name} met {round(eps,2)}min de trop pour aller de la tâche {tasks_id[i2]} à la tâche {tasks_id[i1]}")
 
-            # 4 bis- temps nécessaire à la pause midi
+            # 4 bis- 1h lunch break time
             if T_i_startTime[i1] < lunch_time and T_i_startTime[i2] > lunch_time:
                 if T_i_startTime[i1] + tasks[i1_bis].TaskDuration > 13*60:
                     print(f"ERROR 4.4 : {employee_name} fini la tâche T{i1_bis} trop tard pour manger")
@@ -134,14 +134,14 @@ def verification(country, epsilon=0.05):
                 if eps < 60:
                     print(f"ERROR 4.6 : {employee_name} n'a pas le temps de manger entre les tâches T{i1_bis} et T{i2_bis}")
 
-            # 7- lieu de l'indisponibilité de l'employé
+            # 7- employee unavailability location
             for unavailability in employees[k].Unavailabilities :
                 if T_i_startTime[i1] < unavailability.Start and T_i_startTime[i2] > unavailability.Start:
                     eps2 = T_i_startTime[i2] - (T_i_startTime[i1] + tasks[i1_bis].TaskDuration)
                     if eps2 < trajet(tasks[i1_bis], unavailability) + (unavailability.End - unavailability.Start)+ trajet(unavailability , tasks[i2_bis]):
                         print(f"ERROR 7.3 : {employee_name} n'est pas au bon endroit pendant sa période d'indisponibilité")
 
-        # 8- trajet dernièrer tâche / dépôt
+        # 8- last travel from task to depot
         iN = sorted_indices[-1]
         iN_bis = sorted_indices_bis[-1]
         if T_i_startTime[iN] > employees[k].WorkingEndTime - trajet(tasks[iN_bis], employees[k]):
@@ -152,9 +152,9 @@ def verification(country, epsilon=0.05):
 
 
 ##############################################################
-# DIAGRAMME DE GANTT
+# GANTT DIAGRAM
 def gantt_diagram(country):
-    employees,tasks = readingData(country)          # le dépôt n'est pas dans les tâches
+    employees,tasks = readingData(country)          # depot is not a task
     employees = employees[1:]
     tasks = tasks[1:]
     number_of_employees = len(employees)
@@ -184,14 +184,14 @@ def gantt_diagram(country):
         T_i_startTime = decision[decision['employeeName']==employee_name].loc[:,'startTime'].values
         if len(T_i_startTime) == 0:
             continue
-        sorted_indices = np.argsort(T_i_startTime)          # indices uniquement des tâches effectuées par le technicien k
-        sorted_indices_bis = []                             # utilisables sur tasks (ensemble de toutes les tâches)
+        sorted_indices = np.argsort(T_i_startTime)          # indexs of tasks accomplished only by employee k
+        sorted_indices_bis = []                             # set of all the tasks
         for i in sorted_indices:
             for j in range(number_of_tasks):
                 if tasks[j].TaskId == tasks_id[i]:
                     sorted_indices_bis.append(j)
 
-        # lunch times
+        # lunch time
         lunch_time = 0
         if len(lunch[lunch['employeeName']==employee_name].loc[:,'lunchBreakStartTime'].values)==1:
             lunch_time = lunch[lunch['employeeName']==employee_name].loc[:,'lunchBreakStartTime'].values[0]
@@ -228,7 +228,7 @@ def gantt_diagram(country):
         else: 
             travel_times[k].append((end_times[-1],trajet(employees[k],tasks[sorted_indices_bis[-1]])))
 
-        # indisponibilités
+        # unavailabilities
         for unavailability in employees[k].Unavailabilities:
             unavailabilities_times[k].append((unavailability.Start, unavailability.End-unavailability.Start))
 
@@ -243,7 +243,7 @@ def gantt_diagram(country):
     plt.vlines([12*60, 14*60], 0, 10*(number_of_employees+1), 'red', 'dashed', alpha=0.5)
     plt.savefig('gantt_phase2/{}.png'.format(country))
 
-    ## 2ème plot
+    ## 2nd plot
     fig2,blab=plt.subplots()
     blab.set_ylim(0,10*len(tasks)+10)
     fig2.suptitle('{}_tasks'.format(country))
@@ -256,7 +256,7 @@ def gantt_diagram(country):
         task_id = tasks[i].TaskId
         T_i_startTime = decision[decision['taskId']==task_id].loc[:,'startTime'].values
         
-        # change la couleur si la tâche Ti est faite
+        # change the color if task Ti is done
         color = 'gray'
         if decision[decision['taskId']==task_id].loc[:,'performed'].values == 1:
             employee_name = decision[decision['taskId']==task_id].loc[:,'employeeName'].values[0]
